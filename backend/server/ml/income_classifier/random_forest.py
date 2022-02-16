9b7441c3535090c59c7ef1ba899109ac21715b9e
maxim <urbaevmax@gmail.com>
2022-02-16 14:34:24 +0300

    new path to jupiter notebook file

--- backend/server/ml/income_classifier/random_forest.py
+++ backend/server/ml/income_classifier/random_forest.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class RandomForestClassifier:
     def __init__(self):
-        path_to_artifacts = "../../ml_research/"
+        path_to_artifacts = "/code/ml_research/"
         self.values_fill_missing =  joblib.load(path_to_artifacts + "train_mode.joblib")
         self.encoders = joblib.load(path_to_artifacts + "encoders.joblib")
         self.model = joblib.load(path_to_artifacts + "random_forest.joblib")
